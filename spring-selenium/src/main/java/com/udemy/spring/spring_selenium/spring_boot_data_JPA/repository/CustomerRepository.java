41b0b909756c714c6de258374126509a47fb60c0
Tanuj Lakshakar <tanuj.lakshakar@outlook.com>
2024-08-26 07:55:45 +0530

    Data Driven Testing Using Spring Data JPA

--- spring-selenium/src/main/java/com/udemy/spring/spring_selenium/spring_boot_data_JPA/repository/CustomerRepository.java
+++ spring-selenium/src/main/java/com/udemy/spring/spring_selenium/spring_boot_data_JPA/repository/CustomerRepository.java
@@ -4,6 +4,9 @@ import com.udemy.spring.spring_selenium.spring_boot_data_JPA.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.List;
+
 /**
  * @Repository annotation in Spring is used to indicate that a class serves as a repository.
  * It signifies that the decorated class provides mechanisms for storage, retrieval, update, deletion, and search operations on objects.
@@ -12,4 +15,7 @@ import org.springframework.stereotype.Repository;
  * By using @Repository, Spring will automatically detect and manage instances of CustomerRepository.
  */
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {}
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+    List<Customer> findByFirstNameStartingWith(String startsWith);
+    List<Customer> findByDobBetween(Date from, Date to);
+}
--- spring-selenium/src/test/java/com/udemy/spring/spring_selenium/spring_boot_data_JPA/CustomerVisaTest.java
+++ spring-selenium/src/test/java/com/udemy/spring/spring_selenium/spring_boot_data_JPA/CustomerVisaTest.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.ITestContext;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.sql.Date;
@@ -23,6 +24,7 @@ public class CustomerVisaTest extends SpringBaseTestNGTests {
     private VisaRegistrationPage registrationPage;
 
     @Test
+    @Ignore
     public void visaTest1() {
         // Print number of records in user_visa.csv
         System.out.println("# Records: " + this.repository.findAll().size());
@@ -36,14 +38,15 @@ public class CustomerVisaTest extends SpringBaseTestNGTests {
      * Submit VISA for 3 customers
      */
     @Test
-    public void visaTest3() {
+    @Ignore
+    public void visaTest2() {
         List<Customer> customers = this.repository.findAll() // This method retrieves all records from the repository
                 .stream() // This converts the collection of records into a stream. Streams allow you to process elements sequentially or in parallel.
                 .limit(3) // This limits the stream to the first three elements. In other words, it selects only the first three records from the stream.
                 .collect(Collectors.toList()); // This collects the limited stream back into a list. The resulting list will contain at most three records.
 
         for (Customer u : customers) {
-            logger.info("Inserting record of #"+u.getId());
+            logger.info("Submitting record of #"+u.getId());
             this.registrationPage.goTo();
             this.registrationPage.setNames(u.getFirstName(), u.getLastName());
             this.registrationPage.setCountryFromAndTo(u.getFromCountry(), u.getToCountry());
@@ -57,31 +60,108 @@ public class CustomerVisaTest extends SpringBaseTestNGTests {
         }
     }
 
-//    @DataProvider
-//    @Test(dataProvider = "getData")
-//    public void visaTest2(Customer u) {
-//        this.registrationPage.goTo();
-//        this.registrationPage.setNames(u.getFirstName(), u.getLastName());
-//        this.registrationPage.setCountryFromAndTo(u.getFromCountry(), u.getToCountry());
-//        this.registrationPage.setBirthDate(u.getDob().toLocalDate());
-//        this.registrationPage.setContactDetails(u.getEmail(), u.getPhone());
-//        this.registrationPage.setComments(u.getComments());
-//        this.registrationPage.submit();
-//
-//        logger.info("Request confirmation # INFO : " + this.registrationPage.getConfirmationNumber());
-//        logger.warn("Request confirmation # WARN : " + this.registrationPage.getConfirmationNumber());
-//
-//    }
-//
-//    @DataProvider
-//    public Object[][] getData(ITestContext context) {
-//        return this.repository.findByDobBetween(
-//                        Date.valueOf("2000-01-01"),
-//                        Date.valueOf("2010-01-01")
-//                )
-//                .stream()
-//                .limit(3)
-//                .map(o -> new Customer[]{o})
-//                .toArray(Object[][]::new);
-//    }
+    /**
+     * Submit the records of users whose firstName starts-with "Ch" from user-visa.csv
+     */
+    @Test
+    @Ignore
+    public void visaTest3() {
+        List<Customer> customers = this.repository.findByFirstNameStartingWith("Ch") // This method retrieves all records from the repository
+                .stream() // This converts the collection of records into a stream. Streams allow you to process elements sequentially or in parallel.
+                .collect(Collectors.toList()); // This collects the limited stream back into a list. The resulting list will contain at most three records.
+
+        for (Customer u : customers) {
+            logger.info("Submitting record of #"+u.getId());
+            this.registrationPage.goTo();
+            this.registrationPage.setNames(u.getFirstName(), u.getLastName());
+            this.registrationPage.setCountryFromAndTo(u.getFromCountry(), u.getToCountry());
+            this.registrationPage.setBirthDate(u.getDob().toLocalDate());
+            this.registrationPage.setContactDetails(u.getEmail(), u.getPhone());
+            this.registrationPage.setComments(u.getComments());
+            this.registrationPage.submit();
+
+            logger.info("Request confirmation # INFO : " + this.registrationPage.getConfirmationNumber());
+            logger.warn("Request confirmation # WARN : " + this.registrationPage.getConfirmationNumber());
+        }
+    }
+
+    @Ignore
+    @DataProvider
+    @Test(dataProvider = "getData")
+    public void visaTest4(Customer u) {
+        this.registrationPage.goTo();
+        this.registrationPage.setNames(u.getFirstName(), u.getLastName());
+        this.registrationPage.setCountryFromAndTo(u.getFromCountry(), u.getToCountry());
+        this.registrationPage.setBirthDate(u.getDob().toLocalDate());
+        this.registrationPage.setContactDetails(u.getEmail(), u.getPhone());
+        this.registrationPage.setComments(u.getComments());
+        this.registrationPage.submit();
+
+        logger.info("Request confirmation # INFO : " + this.registrationPage.getConfirmationNumber());
+        logger.warn("Request confirmation # WARN : " + this.registrationPage.getConfirmationNumber());
+
+    }
+
+    @DataProvider
+    public Object[][] getData(ITestContext context) {
+        return this.repository.findByDobBetween(
+                        Date.valueOf(context.getCurrentXmlTest().getParameter("dobStartFrom")),
+                        Date.valueOf(context.getCurrentXmlTest().getParameter("dobEndTo"))
+                )
+                .stream()
+                .limit(3)
+                .map(o -> new Customer[]{o})
+                .toArray(Object[][]::new);
+    }
+
+    /**
+     * Verify that new records are inserted in Customer DB via data.sql and respective dependencies are defined under application.properties
+     */
+    @Test
+    public void visaTest5() {
+        // Print number of records in user_visa.csv
+        System.out.println("# Records: " + this.repository.findAll().size());
+        // Print the record of Id = 101
+        this.repository.findById(101).ifPresent(u -> System.out.println("Record[101]: " + u.getFirstName() + " " + u.getLastName()));
+        // Print the record of Id = 102. This won't print anything as this record doesn't exist in user_visa.csv
+        this.repository.findById(102).ifPresent(u -> System.out.println("Record[102]: " + u.getFirstName() + " " + u.getLastName()));
+        // Print the record of Id = 103. This won't print anything as this record doesn't exist in user_visa.csv
+        this.repository.findById(103).ifPresent(u -> System.out.println("Record[103]: " + u.getFirstName() + " " + u.getLastName()));
+        // Print the record of Id = 104. This won't print anything as this record doesn't exist in user_visa.csv
+        this.repository.findById(104).ifPresent(u -> System.out.println("Record[104]: " + u.getFirstName() + " " + u.getLastName()));
+
+        logger.info("Deleting record: [104]");
+        this.repository.deleteById(104); // Deleting 1 record by ID
+
+        // Print number of records in user_visa.csv
+        System.out.println("# Records: " + this.repository.findAll().size());
+        // Print the record of Id = 101
+        this.repository.findById(101).ifPresent(u -> System.out.println("Record[101]: " + u.getFirstName() + " " + u.getLastName()));
+        // Print the record of Id = 102
+        this.repository.findById(102).ifPresent(u -> System.out.println("Record[102]: " + u.getFirstName() + " " + u.getLastName()));
+        // Print the record of Id = 103
+        this.repository.findById(103).ifPresent(u -> System.out.println("Record[103]: " + u.getFirstName() + " " + u.getLastName()));
+        // Print the record of Id = 104
+        this.repository.findById(104).ifPresent(u -> System.out.println("Record[104]: " + u.getFirstName() + " " + u.getLastName()));
+
+        /**
+         * Inserting new record - [104]
+         */
+        logger.info("Inserting record: [104]");
+        Customer newCustomer = new Customer();
+        newCustomer.setId(104);
+        newCustomer.setFirstName("Rakefet");
+        newCustomer.setLastName("Shimony");
+        newCustomer.setFromCountry("Israel");
+        newCustomer.setToCountry("India");
+        newCustomer.setDob(Date.valueOf("2011-05-31"));
+        newCustomer.setPhone("1-572-883-1594");
+        newCustomer.setEmail("rakefet.shimony@nobody.com");
+        newCustomer.setComments("To insert data using Spring Data JPA with an H2 database");
+        this.repository.save(newCustomer);
+
+
+        // Print the record of Id = 104
+        this.repository.findById(104).ifPresent(u -> System.out.println("Record[104]: " + u.getFirstName() + " " + u.getLastName() + " " + u.getEmail() + " " + u.getPhone() + " " + u.getDob()));
+    }
 }
