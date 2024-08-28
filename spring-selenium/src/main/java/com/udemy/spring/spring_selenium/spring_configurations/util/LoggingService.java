9172d907becf85379af977c600a061ffaef76da3
Tanuj Lakshakar <tanuj.lakshakar@outlook.com>
2024-08-28 06:48:41 +0530

    Implement Logging

--- spring-selenium/src/main/java/com/udemy/spring/spring_selenium/spring_configurations/util/LoggingService.java
+++ spring-selenium/src/main/java/com/udemy/spring/spring_selenium/spring_configurations/util/LoggingService.java
@@ -10,11 +10,32 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
  * This annotation allows you to conditionally create a bean based on an expression.
  * It checks whether the specified expression evaluates to true before creating the bean.
  * You can use it to control bean creation based on properties, environment variables, or other conditions.
+ *
+ * If either property is missing or evaluates to false, the bean won’t be created.
+ * The values for these properties are defined in your application.properties file.
  */
-@ConditionalOnExpression("${log.enabled:true} && ${logging.level.root:INFO}")
+@ConditionalOnExpression("${log.enabled:true} && ${logging.level.root:INFO}") // These values are defined under application.properties
 public class LoggingService {
+    /**
+     * Logging Levels
+     *
+     * ● ERROR
+     * ● WARN
+     * ● INFO
+     * ● DEBUG
+     * ● TRACE
+     */
     private static final Logger logger = LoggerFactory.getLogger(LoggingService.class);
+
     public void logMessage(String message) {
         logger.info(message);
+    }
+
+    public void info(String message) {
+        logger.info("# INFO : " + message);
+    }
+
+    public void warn(String message) {
+        logger.warn("# WARN : " + message);
     }
 }
--- spring-selenium/src/test/java/com/udemy/spring/spring_selenium/spring_boot_data_JPA/CustomerVisaTest.java
+++ spring-selenium/src/test/java/com/udemy/spring/spring_selenium/spring_boot_data_JPA/CustomerVisaTest.java
@@ -4,8 +4,7 @@ import com.udemy.spring.spring_selenium.SpringBaseTestNGTests;
 import com.udemy.spring.spring_selenium.spring_boot_data_JPA.entity.Customer;
 import com.udemy.spring.spring_selenium.spring_boot_data_JPA.repository.CustomerRepository;
 import com.udemy.spring.spring_selenium.spring_boot_data_JPA.visa.VisaRegistrationPage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.udemy.spring.spring_selenium.spring_configurations.util.LoggingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.ITestContext;
 import org.testng.annotations.DataProvider;
@@ -17,7 +16,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CustomerVisaTest extends SpringBaseTestNGTests {
-    private static final Logger logger = LoggerFactory.getLogger(CustomerVisaTest.class);
+//    private static final Logger logger = LoggerFactory.getLogger(CustomerVisaTest.class); // TODO: This works. Enable this line and disable below LoggingService line
+    @Autowired
+    private LoggingService logger; // It is a bean
+    //private LoggingService logger = new LoggingService(); // No need as we already created LoggerConfig to allow Spring to manage it
+
     @Autowired
     private CustomerRepository repository;
     @Autowired
@@ -99,7 +102,6 @@ public class CustomerVisaTest extends SpringBaseTestNGTests {
 
         logger.info("Request confirmation # INFO : " + this.registrationPage.getConfirmationNumber());
         logger.warn("Request confirmation # WARN : " + this.registrationPage.getConfirmationNumber());
-
     }
 
     @DataProvider
@@ -109,9 +111,9 @@ public class CustomerVisaTest extends SpringBaseTestNGTests {
                         Date.valueOf(context.getCurrentXmlTest().getParameter("dobEndTo"))
                 )
                 .stream()
-                .limit(3)
-                .map(o -> new Customer[]{o})
-                .toArray(Object[][]::new);
+                .limit(3) // limits the stream to the first 3 elements.
+                .map(o -> new Customer[]{o}) // maps each element to an array of Customer objects.
+                .toArray(Object[][]::new); // converts the stream to a 2D array of Object.
     }
 
     /**
@@ -120,36 +122,36 @@ public class CustomerVisaTest extends SpringBaseTestNGTests {
     @Test
     public void visaTest5() {
         // Print number of records in user_visa.csv
-        System.out.println("# Records: " + this.repository.findAll().size());
+        logger.info("# Records: " + this.repository.findAll().size());
         // Print the record of Id = 101
-        this.repository.findById(101).ifPresent(u -> System.out.println("Record[101]: " + u.getFirstName() + " " + u.getLastName()));
+        this.repository.findById(101).ifPresent(u -> logger.info("Record[101]: " + u.getFirstName() + " " + u.getLastName()));
         // Print the record of Id = 102. This won't print anything as this record doesn't exist in user_visa.csv
-        this.repository.findById(102).ifPresent(u -> System.out.println("Record[102]: " + u.getFirstName() + " " + u.getLastName()));
+        this.repository.findById(102).ifPresent(u -> logger.info("Record[102]: " + u.getFirstName() + " " + u.getLastName()));
         // Print the record of Id = 103. This won't print anything as this record doesn't exist in user_visa.csv
-        this.repository.findById(103).ifPresent(u -> System.out.println("Record[103]: " + u.getFirstName() + " " + u.getLastName()));
+        this.repository.findById(103).ifPresent(u -> logger.info("Record[103]: " + u.getFirstName() + " " + u.getLastName()));
         // Print the record of Id = 104. This won't print anything as this record doesn't exist in user_visa.csv
-        this.repository.findById(104).ifPresent(u -> System.out.println("Record[104]: " + u.getFirstName() + " " + u.getLastName()));
+        this.repository.findById(104).ifPresent(u -> logger.info("Record[104]: " + u.getFirstName() + " " + u.getLastName()));
 
         logger.info("Deleting record: [104]");
         this.repository.deleteById(104); // Deleting 1 record by ID
 
         // Print number of records in user_visa.csv
-        System.out.println("# Records: " + this.repository.findAll().size());
+        logger.info("# Records: " + this.repository.findAll().size());
         // Print the record of Id = 101
-        this.repository.findById(101).ifPresent(u -> System.out.println("Record[101]: " + u.getFirstName() + " " + u.getLastName()));
+        this.repository.findById(101).ifPresent(u -> logger.info("Record[101]: " + u.getFirstName() + " " + u.getLastName()));
         // Print the record of Id = 102
-        this.repository.findById(102).ifPresent(u -> System.out.println("Record[102]: " + u.getFirstName() + " " + u.getLastName()));
+        this.repository.findById(102).ifPresent(u -> logger.info("Record[102]: " + u.getFirstName() + " " + u.getLastName()));
         // Print the record of Id = 103
-        this.repository.findById(103).ifPresent(u -> System.out.println("Record[103]: " + u.getFirstName() + " " + u.getLastName()));
+        this.repository.findById(103).ifPresent(u -> logger.info("Record[103]: " + u.getFirstName() + " " + u.getLastName()));
         // Print the record of Id = 104
-        this.repository.findById(104).ifPresent(u -> System.out.println("Record[104]: " + u.getFirstName() + " " + u.getLastName()));
+        this.repository.findById(104).ifPresent(u -> logger.info("Record[104]: " + u.getFirstName() + " " + u.getLastName()));
 
         /**
          * Inserting new record - [104]
          */
-        logger.info("Inserting record: [104]");
+        logger.info("Inserting record: [444]");
         Customer newCustomer = new Customer();
-        newCustomer.setId(104);
+        newCustomer.setId(444);
         newCustomer.setFirstName("Rakefet");
         newCustomer.setLastName("Shimony");
         newCustomer.setFromCountry("Israel");
@@ -160,8 +162,19 @@ public class CustomerVisaTest extends SpringBaseTestNGTests {
         newCustomer.setComments("To insert data using Spring Data JPA with an H2 database");
         this.repository.save(newCustomer);
 
+        // Print the record of Id = 444
+        this.repository.findById(444).ifPresent(u -> logger.info("Inserted Record[444]: " + u.getFirstName() + " " + u.getLastName() + " " + u.getEmail() + " " + u.getDob()));
 
-        // Print the record of Id = 104
-        this.repository.findById(104).ifPresent(u -> System.out.println("Record[104]: " + u.getFirstName() + " " + u.getLastName() + " " + u.getEmail() + " " + u.getPhone() + " " + u.getDob()));
+        this.registrationPage.goTo();
+        this.registrationPage.setNames(newCustomer.getFirstName(), newCustomer.getLastName());
+        this.registrationPage.setCountryFromAndTo(newCustomer.getFromCountry(), newCustomer.getToCountry());
+        this.registrationPage.setBirthDate(newCustomer.getDob().toLocalDate());
+        this.registrationPage.setContactDetails(newCustomer.getEmail(), newCustomer.getPhone());
+        this.registrationPage.setComments(newCustomer.getComments());
+        this.registrationPage.submit();
+
+        logger.logMessage("Request confirmation  : " + this.registrationPage.getConfirmationNumber());
+        logger.info("Request confirmation # INFO : " + this.registrationPage.getConfirmationNumber());
+        logger.warn("Request confirmation # WARN : " + this.registrationPage.getConfirmationNumber());
     }
 }
